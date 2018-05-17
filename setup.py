621f8b6a5ba777d5ab477085a589b2fb9989fd68
Val <zarnica@gmail.com>
2018-05-17 19:48:46 -0400

    for pypi packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='exoticst',
+    version='1.0.0',
+    packages=['', 'tests'],
+    url='https://github.com/valginer0/exotic-structures.git',
+    license='MIT',
+    author='Val Giner',
+    author_email='valginer0@gmail.com',
+    description='Data structures,  either not present in standard Python libs or are asymptotically more efficient '
+)
