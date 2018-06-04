16f4801d513af295b551c9cd068050c12736c93a
Val <valginer0@gmail.com>
2018-06-03 22:47:04 -0400

    Changing the version to v1.3.1 for publishing on pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='exoticst',
-    version='1.3.0',
+    version='1.3.1',
     author='Val Giner',
     author_email='valginer0@gmail.com',
     description='Data structures, either not present in standard Python libs or are asymptotically more efficient',
